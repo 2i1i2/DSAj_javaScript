32b654eda7ff8e7bcbc1a5223e6555dfcc44aeb7
AGL <nitinkumar42045@gmail.com>
2023-09-09 11:38:16 +0530

    first version

--- Arrays/00_Prob_TwoSum.js
+++ Arrays/00_Prob_TwoSum.js
@@ -1,3 +1,22 @@
+const TwoSum = function(nums,target){
+    //console.log(nums,target)
+    for(let p1 = 0; p1< nums.length; p1++){
+        const numberToFind = target - nums[p1];
+        // console.log(numberToFind);
+        for(p2 = p1+1; p2<nums.length; p2++){
+            if(numberToFind === nums[p2]){
+                return [p1,p2];
+            }
+        }
+    }
+    return null;
+}
+
+const nums = [1,3,7,9,2];
+const target = 11;
+
+const result = TwoSum(nums,target);
+console.log(result);
 
 
 
